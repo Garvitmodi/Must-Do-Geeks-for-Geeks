1a6ad44e48667966bd6a548d4e614a87da2d59d6
Garvitmodi <44116230+Garvitmodi@users.noreply.github.com>
2020-08-17 13:22:18 +0530

    Add files via upload
    https://practice.geeksforgeeks.org/problems/recursively-remove-all-adjacent-duplicates/0

---
+++ str4.cpp
@@ -0,0 +1,39 @@
+    #include <bits/stdc++.h> 
+     
+    using namespace std;
+    
+     
+     
+  
+     
+    int main()
+    {
+     
+     
+    		ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    	 	int i,j,n,m,cont=0,p,q,d,mn,mx,l,r,sum=0,z1,t,k;
+      		
+      		
+      		cin>>t;
+     	
+      		
+      		for(j=0;j<t;j++){
+      			string s;
+      			cin>>s;
+      			n=s.length();
+      		
+      		}
+      		
+      		
+    
+
+      		
+      		
+      	
+      				
+    						
+    	
+    		
+  }
+     
+     
