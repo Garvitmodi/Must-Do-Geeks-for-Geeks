9f67577c2452fe670acd86b25d5f4540b3df640e
Garvitmodi <44116230+Garvitmodi@users.noreply.github.com>
2020-08-17 13:25:56 +0530

    Add files via upload
    https://practice.geeksforgeeks.org/problems/remove-duplicates/0

---
+++ str8.cpp
@@ -0,0 +1,53 @@
+    #include <bits/stdc++.h> 
+     
+    using namespace std;
+       
+   int main()
+ {
+     
+     
+    		ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    	 	int i,j,n,m,cont=0,p,q,d,mn,mx,l,r,sum=0,z1,k,a,b;
+      	
+      		
+      		cin >> k;
+     	
+     		// for \n to be stored in this
+     		//getchar();
+      			string s,t,s1;
+      				cin.ignore();	
+      		for(j=0;j<k;j++){
+      			
+      		
+      			
+      	
+      			getline(cin, s);
+      			
+      			
+      			int arr[257]={0};
+      			
+      			n=s.length();
+      		
+      			for(i=0;i<n;i++){
+      				if(arr[s[i]]==0){
+      				cout<<s[i];			
+      					arr[s[i]]=1;
+      				}
+      			}
+      			
+      			cout<<endl;
+      				
+      	}
+      		
+      		
+    
+
+      		
+      		
+      	
+      				
+    						
+    	
+}
+     
+     
